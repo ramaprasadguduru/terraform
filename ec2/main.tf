dda5c2fc7eca008d91841af8215d07f7e1c4bc55
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-10-04 18:41:58 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -11,3 +11,8 @@ resource "aws_instance" "app_server" {
     Name = "rama"
   }
 }
+ resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  ami           = "ami-03d5c68bab01f3496"
+   instance_type = "t2.micro"
+ }
