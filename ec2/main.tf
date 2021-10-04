ff241d6c94aa7f1c363ae6f02888ffdda4e5408a
zs-ramaprasad <82650543+zs-ramaprasad@users.noreply.github.com>
2021-10-04 18:07:26 +0530

    Update main.tf

--- ec2/main.tf
+++ ec2/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region  = "us-west-2"
 }
 
-resource "aws_instance" "rama" {
-  ami           = "ami-09e67e426f25ce0d7"
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
   instance_type = "t2.micro"
 
   tags = {
