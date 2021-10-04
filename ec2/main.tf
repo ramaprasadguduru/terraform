68f907046e4d10ac0b4bed997bcf67b5735556be
zs-ramprasad <gudururamprasad@gmail.com>
2021-10-04 18:39:34 +0530

    j

--- ec2/main.tf
+++ ec2/main.tf
@@ -13,7 +13,7 @@ resource "aws_instance" "rama" {
 }
 
  resource "aws_instance" "app_server" {
--  ami           = "ami-830c94e3"
-+  ami           = "ami-08d70e59c07c61a3a"
+  ami           = "ami-830c94e3"
+  ami           = "ami-08d70e59c07c61a3a"
    instance_type = "t2.micro"
  }
