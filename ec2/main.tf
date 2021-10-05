fab5b2d22777bf222f5c255a19f398f9bb453423
zs-ramprasad <gudururamprasad@gmail.com>
2021-10-05 17:40:25 +0530

    g

--- ec2/main.tf
+++ ec2/main.tf
@@ -1,29 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
 
-resource "aws_instance" "ram" {
-  ami           = "ami-830c94e3"
+resource "aws_instance" "rama" {
+  ami           = "ami-09e67e426f25ce0d7"
   instance_type = "t2.micro"
 
   tags = {
-<<<<<<< HEAD
-    Name = "prasad"
+    Name = "rama"
   }
 }
 
-=======
-    Name = var.instance_name
-  }
-}
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.ram.id
-}
-
-output "instance_public_ip" {
-  description = "Public IP address of the EC2 instance"
-  value       = aws_instance.ram.public_ip
-}
->>>>>>> 61795362093ce4eae6869df33b43f3b92183d48c
