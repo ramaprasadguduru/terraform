7bf01e288afa9c197b007949b8ff01e7281ce107
zs-ramprasad <gudururamprasad@gmail.com>
2021-10-05 21:44:14 +0530

    hiukh

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "zs-ramprasad"
+    organization = "ramprasad-zelar"
 
     workspaces {
-      name = "ram"
+      name = "vpc"
     }
   }
   required_providers {
@@ -17,36 +17,32 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
-  region  = var.region
+  region     = "us-east-1"
 }
-
-/*create vpc*/
+# Create VPC
+# terraform aws create vpc
 resource "aws_vpc" "vpc" {
-  cidr_block              = var.vpc-cidr
+  cidr_block              = "${var.vpc-cidr}"
   instance_tenancy        = "default"
-  enable_dns_hostnames    = true
 
   tags      = {
-    Name    = "vpc1"
+    Name    = "VPC"
   }
 }
 
-/*creating internet_gateway*/
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.vpc.id
+resource "aws_internet_gateway" "internet-gateway" {
+  vpc_id    = aws_vpc.vpc.id
 
-  tags = {
-    Name = "IGW"
+  tags      = {
+    Name    = "Sample"
   }
 }
 
-/*creating public subnets*/
 resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public-subnet-1-cidr
+  cidr_block              = "${var.public-subnet-1-cidr}"
   availability_zone       = "us-east-1a"
-  
+
   tags      = {
     Name    = "Public Subnet 1"
   }
@@ -54,9 +50,9 @@ resource "aws_subnet" "public-subnet-1" {
 
 resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public-subnet-2-cidr
+  cidr_block              = "${var.public-subnet-2-cidr}"
   availability_zone       = "us-east-1b"
-  
+
   tags      = {
     Name    = "Public Subnet 2"
   }
@@ -64,7 +60,7 @@ resource "aws_subnet" "public-subnet-2" {
 
 resource "aws_subnet" "public-subnet-3" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public-subnet-3-cidr
+  cidr_block              = "${var.public-subnet-3-cidr}"
   availability_zone       = "us-east-1c"
 
   tags      = {
@@ -72,12 +68,12 @@ resource "aws_subnet" "public-subnet-3" {
   }
 }
 
-/*creating public routetable*/
 resource "aws_route_table" "public-route-table" {
   vpc_id       = aws_vpc.vpc.id
+
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
+    gateway_id = aws_internet_gateway.internet-gateway.id
   }
 
   tags       = {
@@ -85,88 +81,75 @@ resource "aws_route_table" "public-route-table" {
   }
 }
 
-/*Associate Public Subnets to routetables*/
 resource "aws_route_table_association" "public-subnet-1-route-table-association" {
   subnet_id           = aws_subnet.public-subnet-1.id
   route_table_id      = aws_route_table.public-route-table.id
 }
-resource "aws_route_table_association" "public-subnet-3-route-table-association" {
+
+resource "aws_route_table_association" "public-subnet-2-route-table-association" {
   subnet_id           = aws_subnet.public-subnet-2.id
   route_table_id      = aws_route_table.public-route-table.id
 }
-resource "aws_route_table_association" "public-subnet-2-route-table-association" {
+
+resource "aws_route_table_association" "public-subnet-3-route-table-association" {
   subnet_id           = aws_subnet.public-subnet-3.id
   route_table_id      = aws_route_table.public-route-table.id
 }
 
-/*creating private subnets*/
 resource "aws_subnet" "private-subnet-1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private-subnet-1-cidr
-  availability_zone       = "us-east-1a"
- 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = "${var.private-subnet-1-cidr}"
+  availability_zone        = "us-east-1a"
+
   tags      = {
-    Name    = "private Subnet 1"
+    Name    = "Private Subnet 1"
   }
 }
 
 resource "aws_subnet" "private-subnet-2" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private-subnet-2-cidr
-  availability_zone       = "us-east-1b"
-  
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = "${var.private-subnet-2-cidr}"
+  availability_zone        = "us-east-1b"
+
   tags      = {
-    Name    = "private Subnet 2"
+    Name    = "Private Subnet 2"
   }
 }
 
 resource "aws_subnet" "private-subnet-3" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.private-subnet-3-cidr
-  availability_zone       = "us-east-1c"
- 
+  vpc_id                   = aws_vpc.vpc.id
+  cidr_block               = "${var.private-subnet-3-cidr}"
+  availability_zone        = "us-east-1c"
+
   tags      = {
-    Name    = "private Subnet 3"
+    Name    = "Private Subnet 3"
   }
 }
 
-/*terraform aws allocate elastic ip*/
-resource "aws_eip" "eip-for-nat-gateway-1" {
-  vpc    = true
-
-  tags   = {
-    Name = "EIP-1"
-  }
-}
-
-
-/*Create Nat Gateway  in Public Subnets*/
-
-resource "aws_nat_gateway" "nat-gateway-1" {
-  allocation_id = aws_eip.eip-for-nat-gateway-1.id
-  subnet_id     = aws_subnet.public-subnet-1.id
-
-  tags   = {
-    Name = "Nat Gateway Public Subnet 1"
-  }
-}
-
-/*Create Private Route Tables and Add Route Through Nat Gateway */
-
-resource "aws_route_table" "private-route-table-1" {
-  vpc_id            = aws_vpc.vpc.id
+resource "aws_route_table" "private-route-table" {
+  vpc_id       = aws_vpc.vpc.id
 
   route {
-    cidr_block      = "0.0.0.0/0"
-    nat_gateway_id  = aws_nat_gateway.nat-gateway-1.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
   }
 
-  tags   = {
-    Name = "Private Route Table 1"
+  tags       = {
+    Name     = "Private Route Table"
   }
 }
 
 resource "aws_route_table_association" "private-subnet-1-route-table-association" {
-  subnet_id         = aws_subnet.private-subnet-1.id
-  route_table_id    = aws_route_table.private-route-table-1.id
+  subnet_id           = aws_subnet.private-subnet-1.id
+  route_table_id      = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-2-route-table-association" {
+  subnet_id           = aws_subnet.private-subnet-2.id
+  route_table_id      = aws_route_table.private-route-table.id
+}
+
+resource "aws_route_table_association" "private-subnet-3-route-table-association" {
+  subnet_id           = aws_subnet.private-subnet-3.id
+  route_table_id      = aws_route_table.private-route-table.id
 }
